db90c41a684e3662862b8a068c7c7474ea874ab3
asilbek <asilbekmirolimov@gmail.com>
2025-03-07 09:57:16 +0500

    changes

--- apps/urls.py
+++ apps/urls.py
@@ -4,4 +4,5 @@ from .views import home_page, about_page
 urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
+    
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
+from .models import News, Category, UserModel, Comment
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'index.html')
+    yangiliklar = News.objects.all()
+    kategoriyalar = Category.objects.all()
+    context = {
+        'yangiliklar': yangiliklar,
+        'kategoriyalar': kategoriyalar
+    }
+    return render(request, 'index.html', context)
 
-# about, category, contact, blog, blog_detail, latest_news, main
+
 def about_page(request):
-    pass
+    return render(request, 'about.html')
 
+
+def news_page(request):
+    yangiliklar = News.objects.all()
+    kategoriyalar = Category.objects.all()
+    context = {
+        'yangiliklar': yangiliklar,
+        'kategoriyalar': kategoriyalar
+    }
+    return render(request, 'blog.html', context)
+
+
+def news_detail(request, pk):
+    yangilik = get_object_or_404(News, pk=pk)
+    kategoriyalar = Category.objects.all()
+    kommentariyalar = Comment.objects.filter(news_id=pk)
+    context = {
+        'yangilik': yangilik,
+        'kategoriyalar': kategoriyalar,
+        'comments': kommentariyalar
+    }
+    return render(request, 'news_detail.html', context)
+
+
+def contact_page(request):
+    return render(request, 'contact.html')
