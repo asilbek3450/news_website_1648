c1c86243966e6b572865e9043c63842ccf2b3e3b
asilbek <asilbekmirolimov@gmail.com>
2025-03-10 00:24:05 +0500

    changes

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import home_page, about_page
+from .views import home_page, about_page, news_page, news_detail, contact_page
 # endpoint -> urllar
 urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
-    
+    path('news/', news_page, name='news'),
+    path('news/<int:pk>/', news_detail, name='news-detail'),
+    path('contact/', contact_page, name='contact'),
+       
 ]
